3990c49ce2cd6a70545e2877f8ce0776661d37c9
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2022-12-20 20:05:36 -0600

    testing

--- assets/js/game-info.js
+++ assets/js/game-info.js
@@ -1,15 +1,15 @@
-///// query elements
-//query elements for rawG
-let gameTitleEl = $(`#gameTitle`);
-let gameRatingEl = $(`#gameRating`);
-let coverArtEl = $(`#coverArt`);
-let summaryEl = $(`#summary`);
-let releaseDateEl = $(`#releaseDate`);
-let genreEl = $(`#genre`);
-let publisherEl = $(`#publisher`);
-let ratingEl = $(`#rating`);
-//query elements for youtube
-let thumbnailEl = $(`#thumbnail`);
-let videoTitleEl = $(`#thumbnail`);
-let channelNameEl = $(`#channelName`);
-let videoDescriptionEl = $(`#videoDescription`);
+// ///// query elements
+// //query elements for rawG
+// let gameTitleEl = $(`#gameTitle`);
+// let gameRatingEl = $(`#gameRating`);
+// let coverArtEl = $(`#coverArt`);
+// let summaryEl = $(`#summary`);
+// let releaseDateEl = $(`#releaseDate`);
+// let genreEl = $(`#genre`);
+// let publisherEl = $(`#publisher`);
+// let ratingEl = $(`#rating`);
+// //query elements for youtube
+// let thumbnailEl = $(`#thumbnail`);
+// let videoTitleEl = $(`#thumbnail`);
+// let channelNameEl = $(`#channelName`);
+// let videoDescriptionEl = $(`#videoDescription`);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,51 @@
-/////Global Variables/////
-//DOM elements
-submitEl = document.querySelector("#submit");
-//API elements
-var displaySearch = "./search-results.html";
+// ///Global Variables/////
+// DOM elements
+// submitEl = document.querySelector("#userInput");
+// //API elements
+// var displaySearch = "./search-results.html";
 
-//Functions
-function init() {
-  getUserPreferences();
-  //TODO: Grab user preferences from local storage
-}
+// Functions
+// function init() {
+//   getUserPreferences();
+//   //TODO: Grab user preferences from local storage
+// }
 
-function getUserPreferences() {
-  //Get's user preferences
-}
+// function getUserPreferences() {
+//   //Get's user preferences
+// }
 
-function setUserPreferences() {
-  //Store form results into local storage
-}
+// function setUserPreferences() {
+//   //Store form results into local storage
+// }
 
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-}
+// function handleSearchFormSubmit(event) {
+//   event.preventDefault();
 
-/////Event Listeners/////
-submitButton.addEventListener("click", handleSearchFormSubmit);
+//   l
+// }
+
+// /////Event Listeners/////
+// submitButton.addEventListener("click", handleSearchFormSubmit);
+
+//base link
+//https://api.rawg.io/api/
+//468ca2a => stash
+let key = "545ccd805cac419baadf398d05033da5"
+
+$("#searchButton").on("click", function(event) {
+    event.preventDefault();
+    let inputVal = $("#userInput").val();
+    console.log(inputVal);
+    fetch("https://api.rawg.io/api/games?genres=" + inputVal +"&page_size=6&key=" + key)
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+})
+
+// function test() {
+//     fetch("https://api.rawg.io/api/games?genres=action&key=545ccd805cac419baadf398d05033da5")
+//     .then((res) => res.json())
+//     .then((res) => console.log(res))
+// }
+
+// test()
+
