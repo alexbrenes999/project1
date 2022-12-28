f9d87b08c97f33a53dce9ffd6fb089e6d1bbf11a
stevendepauw <88113901+stevendepauw@users.noreply.github.com>
2022-12-27 23:34:46 -0600

    got all data from rawg api properly dynamically populating the page

--- assets/js/game-info.js
+++ assets/js/game-info.js
@@ -1,13 +1,14 @@
 $(function () {
     let key = "545ccd805cac419baadf398d05033da5"
+
     let furtherDetails = (new URL(document.location)).searchParams;
     let game = furtherDetails.get("title");
     let gameId = furtherDetails.get("gameId");
+
     let summaryEl = $("#summary");
     let gameTitleEl = $("#gameTitle");
     let gameRatingEl = $("#gameRating")
     let releaseDateEl = $("#releaseDate");
-    let genreEl = $("#genre");
     let publisherEl = $("#publisher");
     let ratingEl = $("#rating");
     let coverArtEl = $("#coverArt");
@@ -18,21 +19,18 @@ $(function () {
     console.log(gameId);
 
     function setNonPub(object) {
-        gameTitleEl.text(object.results[0].name);
-        gameRatingEl.text("Rated: " + object.results[0].esrb_rating.name);
-        coverArtEl.attr("src", object.results[0].background_image
+        console.log(object)
+        gameTitleEl.text(object.name);
+        gameRatingEl.text("Rated: " + object.esrb_rating.name);
+        coverArtEl.attr("src", object.background_image
         )
-        releaseDateEl.text("Released: " + object.results[0].released);
-        // publisherEl.text(object.results.);
-        ratingEl.text("Rating: " + object.results[0].rating);
-        // summaryEl.text(object.results.);
-        //do we want genre considering thats what they searched originally to even get here
-        // genreEl.text(object.results.);
+        releaseDateEl.text("Released: " + object.released);
+        publisherEl.text("Publisher: " + object.publishers[0].name);
+        ratingEl.text("Rating: " + object.rating);
+        summaryEl.text(object.description_raw);
     }
 
-    // gets all the non publisher information and dynamcally sets the text for each
-    //doesnt return the response you would expect based on the docs at https://api.rawg.io/docs/#operation/games_read
-    fetch("https://api.rawg.io/api/games?id=" + gameId + "&page_size=1&key=" + key)
+    fetch("https://api.rawg.io/api/games/" + gameId + "?key=" + key)
         .then(function (res) {
             return res.json();
         })
@@ -41,21 +39,7 @@ $(function () {
             console.log(data);
         })
 
-
-    //api gets and sets the publisher of the game
-    // doesnt return the response that the docs seem to say it should at https://api.rawg.io/docs/#operation/publishers_read
-    // https://rawg.io/api/games/portal-2/collections
-
-    // fetch("https://api.rawg.io/api/games?id=" + gameId + "&key=" + key)
-    //     .then(function (res) {
-    //         return res.json();
-    //     })
-    //     .then(function (data) {
-
-    //         console.log(data);
-    //     })
-
-    //query elements for youtube
+    // query elements for youtube
     let videoContainerEl = (`#videoContainer`);
     let videoTitleEl = (`#videoTitle`);
     let channelNameEl = (`#channelName`);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,5 @@
-
-//base link
-//https://api.rawg.io/api/
-//468ca2a => stash
-// let key = "545ccd805cac419baadf398d05033da5"
-
 $("#searchButton").on("click", function (event) {
     event.preventDefault();
     let inputVal = $("#userInput").val();
     location.replace(`search-results.html?genre=${inputVal}`)
 })
-
-
-
-
